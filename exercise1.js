599987d4bb0ad5dd6ca967bd91dd2db9f19420b1
Aantor Kar <aantor.kar10@gmail.com>
2025-10-25 15:20:11 +0530

    file sorter

---
+++ exercise1.js
@@ -0,0 +1,17 @@
+import fs from "fs/promises";
+import fsn from "fs";
+import path from "path";
+const basepath = "C:\\Users\\Admin\\Desktop\\backend";
+let files = await fs.readdir(basepath);
+for (const item of files) {
+  let ext = item.split(".")[item.split(".").length - 1];
+  console.log("running for",ext)
+  if (ext != "js" && ext != "json" && item.split(".").length > 1) {
+    if (fsn.existsSync(path.join(basepath, ext))) {
+      fs.rename(path.join(basepath, item), path.join(basepath, ext, item));
+    } else {
+      fs.mkdir(ext);
+      fs.rename(path.join(basepath, item), path.join(basepath, ext, item));
+    }
+  }
+}
